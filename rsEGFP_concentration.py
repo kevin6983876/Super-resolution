e7cf3c5d806290f8d5ee981e785b39ff5233773c
SHIHHUAN <kevin6983876@gmail.com>
2021-08-08 12:52:00 +0800

    added file reading io

--- rsEGFP_concentration.py
+++ rsEGFP_concentration.py
@@ -9,7 +9,7 @@ def rsEGFP(y, t, b, c):
     dydt = [-k_p*on + k_m*off, k_p*on-k_m*off]
     return dydt
 k_p = 0.5
-k_m = 0.5
+k_m = 0
 y0 = [0.4, 0.5]
 # We will generate a solution at 101 evenly spaced 
 # samples in the interval 0 <= t <= 10.
@@ -18,9 +18,50 @@ t = np.linspace(0, 10, 101)
 from scipy.integrate import odeint
 sol = odeint(rsEGFP, y0, t, args=(k_p, k_m))
 import matplotlib.pyplot as plt
-plt.plot(t, sol[:, 0], 'b', label='on(t)')
-plt.plot(t, sol[:, 1], 'g', label='off(t)')
+plt.plot(t, sol[:, 0], 'r', label='on(t)')
+plt.plot(t, sol[:, 1], 'b', label='off(t)')
 plt.legend(loc='best')
 plt.xlabel('t')
 plt.grid()
-plt.show()
+plt.savefig("rsEGFP")  
+
+# Reading experimental data as lists
+raw_data_PBS = []
+with open("PBS.txt") as f:
+    for line in f:
+        for word in line.split():
+            raw_data_PBS.append(float(word)) 
+
+PBS = {}
+species_names = ['time','intensity']
+for name in species_names:
+    PBS[name] = []
+for i in range(int(len(raw_data_PBS)/2)):
+    PBS['time'].append(raw_data_PBS[2*i])
+    PBS['intensity'].append(raw_data_PBS[2*i+1])
+
+raw_data_PA_PBS = []
+with open("PA+PBS.txt") as f:
+    for line in f:
+        for word in line.split():
+            raw_data_PA_PBS.append(float(word)) 
+
+PA_PBS = {}
+for name in species_names:
+    PA_PBS[name] = []
+for i in range(int(len(raw_data_PA_PBS)/2)):
+    PA_PBS['time'].append(raw_data_PA_PBS[2*i])
+    PA_PBS['intensity'].append(raw_data_PA_PBS[2*i+1])
+
+raw_data_PA_FC = []
+with open("PA+FC.txt") as f:
+    for line in f:
+        for word in line.split():
+            raw_data_PA_FC.append(float(word)) 
+
+PA_FC = {}
+for name in species_names:
+    PA_FC[name] = []
+for i in range(int(len(raw_data_PA_FC)/2)):
+    PA_FC['time'].append(raw_data_PA_FC[2*i])
+    PA_FC['intensity'].append(raw_data_PA_FC[2*i+1])
