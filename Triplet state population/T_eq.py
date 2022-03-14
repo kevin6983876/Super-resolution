c3062c3c31ba833c35e672315315c9bbc2ba05d9
SHIHHUAN <kevin6983876@gmail.com>
2022-03-14 09:29:46 +0800

    lambda_3 calculation

---
+++ Triplet state population/T_eq.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+P_origin=0.01
+
+sigma=1.3*10**(-20)
+P=P_origin*2.58*10**(18)
+omega_1=0.24*10**(-6)
+k_12=sigma*P/np.pi/omega_1**2
+k_21=250000000
+k_23=900000
+k_31=500000
+
+T_eq = k_23*k_12/(k_12*(k_23+k_31)+k_31*(k_21+k_23))
+print(T_eq)
---
+++ Triplet state population/lambda_3.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+P_origin=0.001
+
+sigma=1.3*10**(-20)
+P=P_origin*2.58*10**(18)
+omega_1=0.24*10**(-6)
+k_12=sigma*P/np.pi/omega_1**2
+k_21=250000000
+k_23=900000
+k_31=500000
+
+lambda_3 = -k_31-k_12*k_23/(k_12+k_21)
+print(lambda_3/1000000)
